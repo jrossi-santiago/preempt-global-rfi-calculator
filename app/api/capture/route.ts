d9700638a5a59ccb571e1c04612365a638473dae
jrossi-santiago <josephrossi613@gmail.com>
2026-04-16 13:15:17 -0400

    Update route.ts

--- app/api/capture/route.ts
+++ app/api/capture/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { Resend } from 'resend'
 import React from 'react'
 import { renderToBuffer } from '@react-pdf/renderer'
-import { ReportPDF } from '@/components/ReportPDF'
+import { ReportPDF } from '../../components/ReportPDF'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
